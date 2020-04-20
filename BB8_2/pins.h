011d158c3cd876c73ed9554cc4f809824435a23e
Nilayan Paul <nlpl931@gmail.com>
2020-04-20 16:44:36 +0530

    Update pins.h

--- BB8_2/pins.h
+++ BB8_2/pins.h
@@ -1,19 +1,19 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define SERVO PA_8  // Servo
-#define NEO PA_15 // Neopixel decoration
+#define SERVO PB1  // Servo
+#define NEO PA15 // Neopixel decoration
 
-#define MA1 PB_6  // Motor 1
-#define MA2 PB_7
+#define MA1 PB6  // Motor 1
+#define MA2 PB7
 
-#define MB1 PA_8  // Motor 2
-#define MB2 PA_9
+#define MB1 PA8  // Motor 2
+#define MB2 PA9
 
-#define E1  PB_12 // Enable pins
-#define E2  PB_13
+#define E1  PB12 // Enable pins
+#define E2  PB13
 
-#define MH1 PB_14 // Head rotation motor
-#define MH2 PB_15
+#define MH1 PB14 // Head rotation motor
+#define MH2 PB15
 
 #endif
