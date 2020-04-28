1d13a0c5be7f1cea5d77cab3e0d2a0b2285f078d
Nilayan Paul <nlpl931@gmail.com>
2020-04-28 13:20:40 +0530

    Update pins.h
    Added pins for HC-05

--- BB8_2/pins.h
+++ BB8_2/pins.h
@@ -1,19 +1,21 @@
 #ifndef PINS_H
 #define PINS_H
 
+#define BT_TX PB10
+#define BT_RX PB11
 #define SERVO PB1  // Servo
-#define NEO PA15 // Neopixel decoration
+#define NEO   PA15 // Neopixel decoration
 
-#define MA1 PB6  // Motor 1
-#define MA2 PB7
+#define MA1   PB6  // Motor 1
+#define MA2   PB7
 
-#define MB1 PA8  // Motor 2
-#define MB2 PA9
+#define MB1   PA8  // Motor 2
+#define MB2   PA9
 
-#define E1  PB12 // Enable pins
-#define E2  PB13
+#define E1    PB12 // Enable pins
+#define E2    PB13
 
-#define MH1 PB14 // Head rotation motor
-#define MH2 PB15
+#define MH1   PB14 // Head rotation motor
+#define MH2   PB15
 
 #endif
